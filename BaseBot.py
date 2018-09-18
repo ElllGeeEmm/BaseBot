e7ca5d9de2a7dbe60153cfd5efe44e65c0fbe0f7
Robert Morrissy <rbrtmorrissey86@gmail.com>
2018-09-18 18:28:25 -0400

    check db before webscraping

--- BaseBot.py
+++ BaseBot.py
@@ -9,28 +9,36 @@ import settings
 browser = Browser('chrome', headless=True)
 url = 'http://www.espn.com/mlb/scoreboard'
 
-browser.visit(url)
-content = browser.html
-soup = BeautifulSoup(content, 'lxml')
-
-games = soup.findAll('article', {'class':'scoreboard'})
-
 #database linking
 engine = create_engine('sqlite:///games.db')
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-for i in range(len(games)):
-	away = games[i].find('tr', {'class': 'away'}).find('span', {'class': 'sb-team-short'})
-	home = games[i].find('tr', {'class': 'home'}).find('span', {'class': 'sb-team-short'})
-	time = games[i].find('span', {'class': 'time'})
-	new_game = Game(date=datetime.date.today(), time=time.text, away_team=away.text, home_team=home.text)
-	session.add(new_game)
+games_db = session.query(Game).filter(Game.date == datetime.date.today()).all()
+
+while games_db == []:
+
+	browser.visit(url)
+	content = browser.html
+	soup = BeautifulSoup(content, 'lxml')
+
+	games = soup.findAll('article', {'class':'scoreboard'})
+
+
+
+	for i in range(len(games)):
+		away = games[i].find('tr', {'class': 'away'}).find('span', {'class': 'sb-team-short'})
+		home = games[i].find('tr', {'class': 'home'}).find('span', {'class': 'sb-team-short'})
+		time = games[i].find('span', {'class': 'time'})
+		new_game = Game(date=datetime.date.today(), time=time.text, away_team=away.text, home_team=home.text)
+		session.add(new_game)
 	
-	print ("{}@{} {}".format(away.text, home.text, time.text))
+		print ("{}@{} {}".format(away.text, home.text, time.text))
 	
 
-session.commit()
+	session.commit()
+	games_db = session.query(Game).filter(Game.date == datetime.date.today()).all()
 
+for game in games_db:
+	print ("{}@{} {}".format(game.away_team, game.home_team, game.time))
