cf3e4bca4f537dc720008c98cc3c6fa2a46c85a7
Robert Morrissy <rbrtmorrissey86@gmail.com>
2018-09-18 17:55:57 -0400

    final verison before db checks

--- BaseBot.py
+++ BaseBot.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from Models import Base, Game
+import settings
 
 browser = Browser('chrome', headless=True)
 url = 'http://www.espn.com/mlb/scoreboard'
@@ -25,9 +26,11 @@ for i in range(len(games)):
 	away = games[i].find('tr', {'class': 'away'}).find('span', {'class': 'sb-team-short'})
 	home = games[i].find('tr', {'class': 'home'}).find('span', {'class': 'sb-team-short'})
 	time = games[i].find('span', {'class': 'time'})
-	new_game = Game(date=datetime.date.today(), away_team=away.text, home_team=home.text)
+	new_game = Game(date=datetime.date.today(), time=time.text, away_team=away.text, home_team=home.text)
 	session.add(new_game)
 	
-	print ("{}@{}".format(away.text, home.text))
+	print ("{}@{} {}".format(away.text, home.text, time.text))
+	
 
 session.commit()
+
