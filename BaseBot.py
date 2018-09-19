bcd1cc74f363dd6bf79e61b13602a23885fe38f2
Robert Morrissy <rbrtmorrissey86@gmail.com>
2018-09-18 21:43:10 -0400

    added praw dependency, submit functionality

--- BaseBot.py
+++ BaseBot.py
@@ -4,7 +4,10 @@ from bs4 import BeautifulSoup
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from Models import Base, Game
-import settings
+from settings import *
+import praw
+
+
 
 browser = Browser('chrome', headless=True)
 url = 'http://www.espn.com/mlb/scoreboard'
@@ -17,6 +20,8 @@ session = DBSession()
 
 games_db = session.query(Game).filter(Game.date == datetime.date.today()).all()
 
+
+
 while games_db == []:
 
 	browser.visit(url)
@@ -25,8 +30,6 @@ while games_db == []:
 
 	games = soup.findAll('article', {'class':'scoreboard'})
 
-
-
 	for i in range(len(games)):
 		away = games[i].find('tr', {'class': 'away'}).find('span', {'class': 'sb-team-short'})
 		home = games[i].find('tr', {'class': 'home'}).find('span', {'class': 'sb-team-short'})
@@ -34,11 +37,21 @@ while games_db == []:
 		new_game = Game(date=datetime.date.today(), time=time.text, away_team=away.text, home_team=home.text)
 		session.add(new_game)
 	
-		print ("{}@{} {}".format(away.text, home.text, time.text))
-	
+		print ("{}@{} {}".format(away.text, home.text, time.text))	
 
 	session.commit()
 	games_db = session.query(Game).filter(Game.date == datetime.date.today()).all()
+	
+	
+
+reddit = praw.Reddit(client_id = reddit_client_id,
+					 client_secret = reddit_client_secret,
+					 password = reddit_password,
+					 user_agent = 'script in testing by u/ElllGeeEmm',
+					 username = reddit_username)
 
 for game in games_db:
+	reddit.subreddit("PostHereAndIllBanYou").submit(title = "Pre-Game Thread: {}@{} {}".format(game.away_team, game.home_team, game.time),
+													selftext = "{}@{} {}".format(game.away_team, game.home_team, game.time))
+	
 	print ("{}@{} {}".format(game.away_team, game.home_team, game.time))
